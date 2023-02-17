6f276ee45e2f8a90046cf8c2ece035beacbb8d72
Shazreen Seatter <shazseatter12@gmail.com>
2023-02-17 17:29:33 +0000

    test book completed start false - passed

--- tests/book_test.py
+++ tests/book_test.py
@@ -10,3 +10,19 @@ class TestBook (unittest.TestCase):
         expected = "Undoctored"
         actual = self.book.title
         self.assertEqual(expected, actual)
+
+
+    def test_book_has_author(self):
+        expected = "Adam Kay"
+        actual = self.book.author
+        self.assertEqual(expected, actual)
+
+    def test_book_has_author(self):
+        expected = "Adam Kay"
+        actual = self.book.author
+        self.assertEqual(expected, actual)
+
+    def test_book_completed_start_false(self):
+        expected = False
+        actual = self.book.completed
+        self.assertEqual(expected, actual)
