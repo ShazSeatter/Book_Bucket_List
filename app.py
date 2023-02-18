01bc25120a60887e78c5e0f2206389486c28632f
Shazreen Seatter <shazseatter12@gmail.com>
2023-02-18 21:58:03 +0000

    books displaying on books page, able to add new book to books page

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 from controllers.books_controller import books_blueprint
 
 
+
 app = Flask(__name__)
 app.register_blueprint(books_blueprint)
 
 
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title = "Home")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 
 from models.book import Book
+from models.author import Author
 
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
@@ -18,4 +19,45 @@ books_blueprint = Blueprint("books", __name__)
 @books_blueprint.route('/books')
 def books():
     books = book_repository.select_all()
-    return render_template('test.html', all_books = books)
+    return render_template('books/index.html', all_books = books)
+
+# CRUD FUNCTIONALITY FOR CREATING NEW AUTHOR 
+
+# # NEW
+# # GET 'author/new'
+# @books_blueprint.route('/books/authors/new')
+# def new_author():
+#     authors = author_repository.select_all()
+#     return render_template('authors/new.html', all_authors = authors)
+
+
+# # CREATE
+# # POST '/books'
+# @books_blueprint.route('/books', methods=['POST'])
+# def create_author():
+#    first_name = request.form['first-name']
+#    last_name = request.form['last-name']
+#    author = Author(first_name, last_name)
+#    author_repository.save(author)
+#    return redirect('/books')
+
+
+
+# NEW BOOK
+
+# NEW
+# GET 'books/new'
+@books_blueprint.route('/books/new')
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", all_authors = authors)
+
+# CREATE
+# POST '/books'
+@books_blueprint.route('/books', methods=['POST'])
+def create_book():
+    title = request.form['title']
+    author = author_repository.select(request.form['author_id'])
+    book = Book(title, author)
+    book_repository.save(book)
+    return redirect('/books')
